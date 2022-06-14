1f7db2523d31feb871c4cef403b804489b0b986e
Aman Amjad <aman.amjad@gmail.com>
2022-06-14 12:17:19 -0500

    I am not sure what this is,,,

--- Program.cs
+++ Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace ConsoleTest {
+    class Program {
+        static void Main(string[] args) {
+
+            //Write something here to recieve user input
+
+            //Convert user input to an int array here
+        
+            //Put menu for user to choose from here along with any needed code to make the options work
+            
+
+
+        }
+    }
+}
+
+ 
