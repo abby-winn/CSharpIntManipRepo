675f79547ba135412eb217e853783da26047f583
Alan Sudhan <23sudhanalan@gmail.com>
2022-06-14 13:24:03 -0500

    We did it

--- Program.cs
+++ Program.cs
@@ -1,36 +1,74 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
+namespace ConsoleTest
+{
+    class Program
+    {
+        static void display()
+        {
+            Console.WriteLine("What would you like to do?");
+            Console.WriteLine("");
 
-using System;
-
-namespace ConsoleTest {
-    class Program {
-        static void Main(string[] args) {
-
-            Console.WriteLine("How many intergers do you want?");
-            int desiredNum = Console.ReadLine();
-
-            int[] integers = new int[desiredNum];
-
-            for(int i = 0; i < desiredNum; i++){
-                Console.WriteLine("Please type an integer.");
-                int desiredInt = Console.ReadLine();
-                integers[i] == desiredInt;
-            }
-
-
+            Console.WriteLine("To display the whole list of input please type 1");
+            Console.WriteLine("To display every even number please type 2");
+            Console.WriteLine("To display every odd number please type 3");
+            Console.WriteLine("To display every 5th number please type 4");
+            Console.WriteLine("To display the sum of the integers please type 5");
+            Console.WriteLine("To display every number divisible by 3 please type 6");
+            Console.WriteLine("To quit please type 7");
         }
 
-        static void three(int[] tempList){
-            for(int i = 0; i<tempList.length; i++){
-                if(tempList[i]%2 != 0){
+        static void one(int[] integers)
+        {
+            for (int i = 0; i < integers.Length; i++)
+            {
+                Console.WriteLine(integers[i]);
+            }
+        }
+
+
+        static void two(int[] integers)
+        {
+            for (int i = 0; i < integers.Length; i++)
+            {
+                if (integers[i] % 2 == 0)
+                {
                     Console.WriteLine(integers[i]);
                 }
             }
-        static void five(int[] integers){
+
+        }
+
+        static void three(int[] integers)
+        {
+            for (int i = 0; i < integers.Length; i++)
+            {
+                if (integers[i] % 2 != 0)
+                {
+                    Console.WriteLine(integers[i]);
+                }
+            }
+        }
+
+
+        static void four(int[] integers)
+        {
+            if (integers.Length < 5)
+            {
+                for (int i = 4; i < integers.Length; i += 5)
+                {
+                    Console.WriteLine(integers[i]);
+                }
+            }
+
+        }
+
+        static void five(int[] integers)
+        {
 
             int sum = 0;
 
-            for (int i = 0; i < integers.Length; i++){
+            for (int i = 0; i < integers.Length; i++)
+            {
                 sum += integers[i];
             }
 
@@ -38,25 +76,77 @@ namespace ConsoleTest {
 
         }
 
-        static void four(int[] tempList){
-            if(tempList.length<5){
-                for(int i = 4; i<tempList.length; i+=5){
-                    Console.WriteLine(integers[i]);
-            }        
-                }
-            
-        }
+        static void six(int[] integers)
+        {
 
-        static void six(int[] integers){
-
-            for (int i = 0; i < integers.Length; i++){
-                if (integers[i] % 3 == 0){
+            for (int i = 0; i < integers.Length; i++)
+            {
+                if (integers[i] % 3 == 0)
+                {
                     Console.WriteLine(integers[i]);
                 }
             }
-
         }
 
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How many intergers do you want?");
+            int intTemp = Convert.ToInt32(Console.ReadLine());
+
+            int[] integers = new int[intTemp];
+
+            for (int i = 0; i < intTemp; i++)
+            {
+                Console.WriteLine("Please type an integer.");
+                int intTemp2 = Convert.ToInt32(Console.ReadLine());
+                integers[i] = intTemp2;
+            }
+
+            int userInput = 0;
+
+            while (userInput != 7)
+            {
+                Console.WriteLine("");
+                display();
+                userInput = Convert.ToInt32(Console.ReadLine());
+
+                if (userInput == 1)
+                {
+                    one(integers);
+                }
+
+                else if (userInput == 2)
+                {
+                    two(integers);
+                }
+
+                else if (userInput == 3)
+                {
+                    three(integers);
+                }
+
+                else if (userInput == 4)
+                {
+                    four(integers);
+                }
+
+                else if (userInput == 5)
+                {
+                    five(integers);
+                }
+
+                else if (userInput == 6)
+                {
+                    six(integers);
+                }
+
+                else
+                {
+                    Console.WriteLine("Invalid Response: Here are your options.");
+                }
+                Console.WriteLine("");
+            }
+        }
     }
 }
 
